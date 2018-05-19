e16c6b71a568979d233151432ae7678b4fa29599
Gang Qiu <gangqiu0@gmail.com>
2018-05-19 19:16:44 -0400

    reading client id from config file

--- README.md
+++ README.md
@@ -14,5 +14,8 @@ ruby server.rb
 open your browser at the URL localhost:4567
 ```
 
+#### configuration file
+It is necessary to add a `config.yaml` file to the project root, which is ignored by version control. A single `BEHANCE_API_KEY: xxxxxxxx` attribute needs to be added.
+
 ## Credit
 Boilerplate: https://github.com/ilasorsa/bare-sinatra-react
--- server.rb
+++ server.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
 require 'json'
 require 'net/http'
+require 'yaml'
+
+config = YAML::load(File.open('./config.yaml'))
+puts config
 
 get '/' do
   send_file File.expand_path('index.html', settings.public_folder)
@@ -8,13 +12,15 @@ end
 
 get '/api/user/:name/projects' do |name|
   content_type :json
-  client_id = ""
+  client_id = "#{config['BEHANCE_API_KEY']}"
   behance_url = "https://api.behance.net/v2/users/#{name}/projects?client_id=#{client_id}"
   res = Net::HTTP.get_response(URI(behance_url))
   case res
     when Net::HTTPSuccess then
       JSON.parse(res.body).to_json
     else
+      puts behance_url
+      puts res.body
       status 400
   end
 end
